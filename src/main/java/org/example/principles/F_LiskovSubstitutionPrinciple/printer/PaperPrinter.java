eea48a09511c0af41c09e88624aefca0270065be
ahmedmmordi <ahmed.m.hamada2003@gmail.com>
2025-11-11 13:51:01 +0200

    Update Print Formatting

--- src/main/java/org/example/principles/F_LiskovSubstitutionPrinciple/printer/PaperPrinter.java
+++ src/main/java/org/example/principles/F_LiskovSubstitutionPrinciple/printer/PaperPrinter.java
@@ -5,12 +5,12 @@ import org.example.principles.F_LiskovSubstitutionPrinciple.vehicle.Vehicle;
 public class PaperPrinter implements RentalReportPrinter {
     @Override
     public void print(Vehicle vehicle) {
-        System.out.println("\n" + vehicle.getVehicleName() + " Renting Summary:");
-        System.out.println("  - Vehicle ID: " + vehicle.getVehicleId());
-        System.out.println("  - Vehicle Name: " + vehicle.getVehicleName());
-        System.out.println("  - Vehicle Hourly Price: $" + vehicle.getVehicleHourlyPrice());
-        System.out.println("  - Vehicle Hours Rented: " + vehicle.getLastRentalHours());
-        System.out.println("  - Vehicle Total Cost: $" + vehicle.getLastRentalCost());
+        System.out.println("\n" + "Vehicle Renting Summary:");
+        System.out.println("  - ID: " + vehicle.getVehicleId());
+        System.out.println("  - Name: " + vehicle.getVehicleName());
+        System.out.println("  - Hourly Price: $" + vehicle.getVehicleHourlyPrice());
+        System.out.println("  - Hours Rented: " + vehicle.getLastRentalHours());
+        System.out.println("  - Total Cost: $" + vehicle.getLastRentalCost());
         System.out.println();
     }
 }
